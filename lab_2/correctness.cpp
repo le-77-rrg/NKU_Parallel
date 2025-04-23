1d619c384303c7415beb0e836c3a67798c0c9f14
lin_ss <303546297@qq.com>
2025-04-23 17:17:10 +0800

    Add files via upload

---
+++ lab_2/correctness.cpp
@@ -0,0 +1,43 @@
+#include "PCFG.h"
+#include <chrono>
+#include <fstream>
+#include "md5.h"
+#include <iomanip>
+using namespace std;
+using namespace chrono;
+
+// 编译指令如下：
+// g++ correctness.cpp train.cpp guessing.cpp md5.cpp -o test.exe
+
+
+// 通过这个函数，你可以验证你实现的SIMD哈希函数的正确性
+int main()
+{   
+    string inputs[4] = {"bvaisdbjasdkafkasdfnavkjnakdjfejfanjsdnfkajdfkajdfjkwanfdjaknsvjkanbjbjadfajwefajksdfakdnsvjadfasjdvabvaisdbjasdkafkasdfnavkjnakdjfejfanjsdnfkajdfkajdfjkwanfdjaknsvjkanbjbjadfajwefajksdfakdnsvjadfasjdvabvaisdbjasdkafkasdfnavkjnakdjfejfanjsdnfkajdfkajdfjkwanfdjaknsvjkanbjbjadfajwefajksdfakdnsvjadfasjdvabvaisdbjasdkafkasdfnavkjnakdjfejfanjsdnfkajdfkajdfjkwanfdjaknsvjkanbjbjadfajwefajksdfakdnsvjadfasjdva", "bvaisdbjasdkafkasdfnavkjnakdjfejfanjsdnfkajdfkajdfjkwanfdjaknsvjkanbjbjadfajwefajksdfakdnsvjadfasjdvabvaisdbjasdkafkasdfnavkjnakdjfejfanjsdnfkajdfkajdfjkwanfdjaknsvjkanbjbjadfajwefajksdfakdnsvjadfasjdvabvaisdbjasdkafkasdfnavkjnakdjfejfanjsdnfkajdfkajdfjkwanfdjaknsvjkanbjbjadfajwefajksdfakdnsvjadfasjdvabvaisdbjasdkafkasdfnavkjnakdjfejfanjsdnfkajdfkajdfjkwanfdjaknsvjkanbjbjadfajwefajksdfakdnsvjadfasjdva", "bvaisdbjasdkafkasdfnavkjnakdjfejfanjsdnfkajdfkajdfjkwanfdjaknsvjkanbjbjadfajwefajksdfakdnsvjadfasjdvabvaisdbjasdkafkasdfnavkjnakdjfejfanjsdnfkajdfkajdfjkwanfdjaknsvjkanbjbjadfajwefajksdfakdnsvjadfasjdvabvaisdbjasdkafkasdfnavkjnakdjfejfanjsdnfkajdfkajdfjkwanfdjaknsvjkanbjbjadfajwefajksdfakdnsvjadfasjdvabvaisdbjasdkafkasdfnavkjnakdjfejfanjsdnfkajdfkajdfjkwanfdjaknsvjkanbjbjadfajwefajksdfakdnsvjadfasjdva", "bvaisdbjasdkafkasdfnavkjnakdjfejfanjsdnfkajdfkajdfjkwanfdjaknsvjkanbjbjadfajwefajksdfakdnsvjadfasjdvabvaisdbjasdkafkasdfnavkjnakdjfejfanjsdnfkajdfkajdfjkwanfdjaknsvjkanbjbjadfajwefajksdfakdnsvjadfasjdvabvaisdbjasdkafkasdfnavkjnakdjfejfanjsdnfkajdfkajdfjkwanfdjaknsvjkanbjbjadfajwefajksdfakdnsvjadfasjdvabvaisdbjasdkafkasdfnavkjnakdjfejfanjsdnfkajdfkajdfjkwanfdjaknsvjkanbjbjadfajwefajksdfakdnsvjadfasjdva"};
+    bit32 state_serial[4][4];
+    for (int i = 0; i < 4; ++i) {
+        MD5Hash(inputs[i], state_serial[i]);
+    }
+    cout << "串行MD5Hash结果:" << endl;
+    for (int i1 = 0; i1 < 4; i1 += 1)
+    {
+        for(int i2 = 0; i2 < 4; i2 += 1){
+            cout << std::setw(8) << std::setfill('0') << hex << state_serial[i1][i2];
+        }
+        cout << endl;
+    }
+    bit32 *state[4];
+for (int i = 0; i < 4; ++i) {
+    state[i] = new bit32[4];  // 每个输入需要4个状态值
+}
+    MD5Hash_NEON(inputs, (bit32 **)state);
+    cout << "并行MD5Hash_NEON结果:" << endl;
+    for (int i1 = 0; i1 < 4; i1 += 1)
+    {
+        for(int i2 = 0; i2 < 4; i2 += 1){
+        cout << std::setw(8) << std::setfill('0') << hex << state[i1][i2];
+        }
+        cout << endl;
+    }
+    
+}
